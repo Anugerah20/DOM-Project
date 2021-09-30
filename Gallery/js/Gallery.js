a31cdc309d7676e13e8679b5b1ba79206eb38b4a
Nabil Anugerah <nabilpangestu123@gmail.com>
2021-09-30 09:53:31 +0700

    upload project gallery

---
+++ Gallery/js/Gallery.js
@@ -0,0 +1,34 @@
+// Cara Pertama
+/* const contain = document.querySelector('.container');
+const big = document.querySelector('.big-image');
+
+contain.addEventListener('click',function(e) {
+    if(e.target.className == 'thub') {
+        big.src = e.target.src;
+    }
+}); */
+
+// Cara Kedua 
+const contain = document.querySelector('.container');
+const big = document.querySelector('.big-image');
+const thumbs = document.querySelectorAll('.thub');
+
+contain.addEventListener('click',function(e) {
+    if(e.target.className == 'thub') {
+        big.setAttribute('src',e.target.src)
+        big.classList.add('fade');
+        setTimeout(function() {
+            big.classList.remove('fade');
+        },1000);
+
+        // Klik gambar dan hilangkan opacity
+        thumbs.forEach(function(thub) {
+            // if(thumb.classList.contains('active')) {
+            //     thumb.classList.remove('active');
+            // }
+            thub.className = 'thub';
+        });
+
+        e.target.classList.add('active');
+    }
+});
